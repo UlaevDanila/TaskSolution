4b42e1d01e59a05a5eff97878dd6ee3240e3460f
Immortal_342 <ulaevdanila63@gmail.com>
2025-04-07 18:46:38 +0200

    Uploading repo

--- SolutionTask/Program.cs
+++ SolutionTask/Program.cs
@@ -1,24 +1,17 @@
+using SolutionTask.Utils.StockMarketConnectors.BitfinexConnector.Rest;
+using StockMarketConnector.Connectors.Rest;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<IRestConnector, BitfinexRestConnector>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
 
 app.UseRouting();
-
-app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
